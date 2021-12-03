96490ea015219e1c1d0e82cbb9821c9828657b12
Brandon r <branr365@gmail.com>
2021-12-03 15:16:14 +0000

    - Made the potential new piece highlight obay the full bounds of the grid - Fixed casting issues in the grid positioning code

--- Code/Track/TrackPiece.cpp
+++ Code/Track/TrackPiece.cpp
@@ -33,36 +33,46 @@ void TrackPiece::RenderFull(BaseCamera* camera)
 #include "../Maths/Constants.h"
 void TrackPiece::SetNewGridPosition(Vector2D newPos)
 {
-	// Cap to grid
-	if (newPos.x > 64.0f)
-		newPos.x = 64.0f;
-	else if (newPos.x < -64.0f)
-		newPos.x = -64.0f;
-
-	if (newPos.y > 64.0f)
-		newPos.y = 64.0f;
-	else if (newPos.y < -64.0f)
-		newPos.y = -64.0f;
-
 	// Now convert to a 0->128 format
 	newPos.x += 64;
 	newPos.y += 64;
 
 	// Now convert the position to being multiple of the grid size
-	unsigned int gridCellSize = 8;
+	int gridCellSize     = 8;
+	int halfGridCellSize = gridCellSize / 2;
+
 	newPos.x -= ((int)newPos.x % gridCellSize);
 	newPos.y -= ((int)newPos.y % gridCellSize);
 	
-	newPos.x += gridCellSize / 2;
-	newPos.y += gridCellSize / 2;
+	// Offset the grid by half a cell size because the origin is in the centre of the grid
+	newPos.x += halfGridCellSize;
+	newPos.y += halfGridCellSize;
 
 	// Now convert back to being the world space format
 	newPos.x -= 64;
 	newPos.y -= 64;
 
-	mGridPosition.x = floor(newPos.x); 
-	mGridPosition.y = floor(newPos.y);
+	// Floor the value to make it an integer, in the correct rounding direction
+	newPos.x = floorf(newPos.x);
+	newPos.y = floorf(newPos.y);
 
-	DirectX::XMStoreFloat4x4(&mModelMatrix, DirectX::XMMatrixTranslation(mGridPosition.x, 0.0f, 
-		                                                                 mGridPosition.y));
+	// Cap to grid
+	float worldSpaceQuaterGridCap = 64.0f - (float)halfGridCellSize;
+
+	if (newPos.x > worldSpaceQuaterGridCap)
+		newPos.x = worldSpaceQuaterGridCap;
+	else if (newPos.x < -worldSpaceQuaterGridCap)
+		newPos.x = -worldSpaceQuaterGridCap;
+
+	if (newPos.y > worldSpaceQuaterGridCap)
+		newPos.y = worldSpaceQuaterGridCap;
+	else if (newPos.y < -worldSpaceQuaterGridCap)
+		newPos.y = -worldSpaceQuaterGridCap;
+
+	// Apply the new position
+	mGridPosition.x = (int)newPos.x;
+	mGridPosition.y = (int)newPos.y;
+
+	DirectX::XMStoreFloat4x4(&mModelMatrix, DirectX::XMMatrixTranslation((float)mGridPosition.x, 0.0f, 
+		                                                                 (float)mGridPosition.y));
 }
